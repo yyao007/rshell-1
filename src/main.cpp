afbb68d3cdd11aa1c9c79f91a1e6770e6a265b58
rheng001 <rheng001@gmail.com>
2015-04-15 11:22:40 -0700

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <pwd.h>
+
+int look_args(char **);
+void exec_cmd(int, char **);
+bool quit(string);
 
 using namespace std;
 
